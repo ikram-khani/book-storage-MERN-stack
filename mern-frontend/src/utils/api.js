47f8bc63e7788b3a0adfc89aabb687424a99f344
Ikram Khan <ikr302000@gmail.com>
2024-01-19 22:59:12 +0500

    Update api.js

--- mern-frontend/src/utils/api.js
+++ mern-frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5555'; // Update with your backend URL
+const API_BASE_URL = 'https://book-storage-mern-stack-api.vercel.app'; // Update with your backend URL
 
 const api = axios.create({
   baseURL: API_BASE_URL,
