61fead6a2a58cc439c571f42fd9721f5ce07a049
Ikram khan <ikr302000@gmail.com>
2024-01-19 15:38:52 +0500

    adding other routes

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import dotenv from 'dotenv';
 dotenv.config();
 import mongoose from "mongoose";
@@ -72,6 +72,46 @@ try {
     res.status(500).send({message: error.message});
 }
 });
+
+//Route for update a book
+app.put('/books/:id', async (req, res) =>{
+try {
+    if(
+        !req.body.title ||
+        !req.body.author ||
+        !req.body.publishYear
+    ){
+        return res.status(400).send({
+message: 'Send all required fields: title, author, publishYear',
+        });
+    }
+    const {id}= req.params;
+
+    const result = await Book.findByIdAndUpdate(id, req.body);
+    if(!result){
+        return res.status(404).json({message: 'Book not found'});
+    }
+    return res.status(200).send({message: 'Book updated successfully'});
+} catch (error) {
+    console.log(error.message);
+    res.status(500).send({message: error.message});
+}
+});
+
+//Route for delete a book
+app.delete('/books/:id', async (req, res) =>{
+    try {
+        const {id}= req.params;
+        const result = await Book.findByIdAndDelete(id);
+        if(!result){
+            return res.status(404).json({message: 'Book not found'});
+        }
+        return res.status(200).send({message: 'Book deleted successfully'});
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+    });
 
 mongoose.connect(mongoDBURL).then(() => {
 console.log('App connected to database');
