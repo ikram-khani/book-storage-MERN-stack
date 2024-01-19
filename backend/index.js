5f49ab1b0cf9699a1a3fdeaec3ef900a32ad3457
Ikram khan <ikr302000@gmail.com>
2024-01-19 11:29:15 +0500

    connection to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,26 @@
 import express from "express";
-import { PORT } from "./config.js";
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from "mongoose";
 
+const port = process.env.PORT;
+const mongoDBURL = process.env.MONGODB_URI;
 const app = express();
 
-app.get('/', (req, res)=>{
- console.log(req);
- return res.status(234).send('Welcome to MERN Stack');
+app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(234).send('Welcome to MERN Stack');
 })
 
-app.listen(PORT, ()=>{
-    console.log(`App is listening to port: ${PORT}`);
+
+
+mongoose.connect(mongoDBURL).then(() => {
+console.log('App connected to database');
+
+app.listen(port, () => {
+    console.log(`App is listening to port: ${port}`);
+});
+
+}).catch((error) => {
+console.log(error);
 });
